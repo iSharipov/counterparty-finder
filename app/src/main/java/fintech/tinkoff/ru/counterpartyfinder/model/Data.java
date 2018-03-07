d2bf5d3bf088582089b0d7558ce9e7ce75f98843
ilias <iliamsharipov@gmail.com>
2018-03-07 21:59:37 +0300

    realm list added realm plugin verson changed

--- app/src/main/java/fintech/tinkoff/ru/counterpartyfinder/model/Data.java
+++ app/src/main/java/fintech/tinkoff/ru/counterpartyfinder/model/Data.java
@@ -1,8 +1,8 @@
 package fintech.tinkoff.ru.counterpartyfinder.model;
 
 import java.io.Serializable;
-import java.util.List;
 
+import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
@@ -33,13 +33,13 @@ public class Data extends RealmObject implements Serializable {
     private String ogrn;
     private String okpo;
     private String okved;
-    private List<String> okveds;
-    private List<String> authoroties;
-    private List<String> documents;
-    private List<String> licenses;
+    private RealmList<String> okveds = new RealmList<>();
+    private RealmList<String> authoroties = new RealmList<>();
+    private RealmList<String> documents = new RealmList<>();
+    private RealmList<String> licenses = new RealmList<>();
     private Address address;
-    private List<String> phones;
-    private List<String> emails;
+    private RealmList<String> phones = new RealmList<>();
+    private RealmList<String> emails = new RealmList<>();
     private Long ogrn_date;
     private String okved_type;
 }
